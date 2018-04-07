01ec6ded1288c43d0ed06a011dda1c6a1c924094
developerworks <developerworks@163.com>
2018-04-07 21:53:37 +0800

    ios version checks

--- Tools/Classes/Common.swift
+++ Tools/Classes/Common.swift
@@ -3,10 +3,14 @@ import UserNotifications
 
 class Tools: NSObject {
     static func requestUserNotification() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (accepted, error) in
-            if !accepted {
-                print("用户不允许消息通知!")
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (accepted, error) in
+                if !accepted {
+                    print("用户不允许消息通知!")
+                }
             }
+        } else {
+            // Fallback on earlier versions
         }
     }
 }
